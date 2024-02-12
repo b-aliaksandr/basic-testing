6b7dc6382fa741b7d90dd9eaf63d6a2eea379279
barodzichaliaskandr@gmail.com <borodichalexander97@gmail.com>
2024-02-12 21:47:49 +0300

    refactor: change comment

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -3,7 +3,7 @@
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    // Write your test here
+    // I just need more time
   });
 
   test('should subtract two numbers', () => {
