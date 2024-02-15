4946759ebee319a18947b87fb79f8b9137944d04
barodzichaliaskandr@gmail.com <borodichalexander97@gmail.com>
2024-02-15 23:29:43 +0300

    test: implement 'generate linked list from values 1' test case

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,10 +1,22 @@
-// Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
-  // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const values = [1, 2, 3];
+    const linkedList = {
+      value: 1,
+      next: {
+        value: 2,
+        next: {
+          value: 3,
+          next: {
+            value: null,
+            next: null,
+          },
+        },
+      },
+    };
+    expect(linkedList).toStrictEqual(generateLinkedList(values));
   });
 
   // Check match by comparison with snapshot
